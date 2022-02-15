58f8151370d350a9a6b97ee2eaa9831acd52fd37
felix <felix@swixdao.com>
2022-02-15 01:43:32 -0300

    test_1

--- src/server.js
+++ src/server.js
@@ -6,5 +6,5 @@ const app = express();
 app.use(express.json())
 app.use(router)
 
-app.listen(3030, () =>  console.log('Server listening on port 3030'))
+app.listen(8080, () =>  console.log('Server listening on port 8080'))
 
